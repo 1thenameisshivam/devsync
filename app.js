7d2475a1d3d7692503d18df524311d39492eca1a
1thenameisshivam <1thenameisshivam@gmail.com>
2024-09-17 16:55:57 +0530

    database connection done

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { DataBase } from "./src/config/DataBase.js";
+const app = express();
+
+DataBase()
+  .then(() => {
+    console.log("Database connected");
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch((err) => {
+    console.log("database is not able to connect", err);
+  });
---
+++ src/config/DataBase.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+
+export const DataBase = async () => {
+  await mongoose.connect(
+    "mongodb+srv://shivamkumarcs21:tcS3zcVOMsdLgsHH@thunderbolt.e09g8.mongodb.net/"
+  );
+};
