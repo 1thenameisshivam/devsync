0d2e0380777bc5094725f593a758e4c04233446a
1thenameisshivam <1thenameisshivam@gmail.com>
2024-09-17 18:56:35 +0530

    delete user api created

--- app.js
+++ app.js
@@ -15,6 +15,37 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+app.get("/user", async (req, res) => {
+  try {
+    const user = await User.find({ email: req.body.email });
+    if (user.length === 0) {
+      return res.status(404).send("user not found");
+    }
+    res.status(200).send(user);
+  } catch (err) {
+    res.status(400).send("somthing went wrong");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).send(users);
+  } catch (err) {
+    res.status(400).send("somthing went wrong");
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const id = req.body.id;
+  try {
+    await User.findByIdAndDelete(id);
+    res.status(200).send("user deleted successfully");
+  } catch (err) {
+    res.status(400).send("somthing went wrong");
+  }
+});
+
 DataBase()
   .then(() => {
     console.log("Database connected");
