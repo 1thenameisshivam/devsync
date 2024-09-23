5ad1c6eb2e207990434143c0b0bfe628c65a526c
1thenameisshivam <1thenameisshivam@gmail.com>
2024-09-23 12:31:02 +0530

    feed api created with pagination

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,5 @@
 import ConnectionRequest from "../models/connectionRequest.js";
-
+import User from "../models/user.js";
 export const userRequests = async (req, res) => {
   try {
     const { _id } = req.user;
@@ -35,6 +35,34 @@ export const userConnections = async (req, res) => {
     const data = users.map((user) =>
       user.fromUser._id === _id ? user.toUser : user.fromUser
     );
+    res.status(200).send(data);
+  } catch (err) {
+    res.status(500).send({ message: err.message, status: false });
+  }
+};
+
+export const userFeed = async (req, res) => {
+  try {
+    const { _id } = req.user;
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit > 50 ? 50 : limit;
+    const skip = (page - 1) * limit;
+
+    const allUsers = await ConnectionRequest.find({
+      $or: [{ fromUser: _id }, { toUser: _id }],
+    }).select("fromUser toUser");
+    const hideUser = new Set();
+    allUsers.forEach((user) => hideUser.add(user.fromUser).add(user.toUser));
+    const data = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUser) } },
+        { _id: { $ne: { _id } } },
+      ],
+    })
+      .select("firstName lastName email createdAt")
+      .skip(skip)
+      .limit(limit);
     res.status(200).send(data);
   } catch (err) {
     res.status(500).send({ message: err.message, status: false });
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -3,11 +3,13 @@ import { isAuthenticated } from "../middlewares/isAuthenticated.js";
 import {
   userRequests,
   userConnections,
+  userFeed,
 } from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
 userRouter.get("/requests", isAuthenticated, userRequests);
 userRouter.get("/connections", isAuthenticated, userConnections);
+userRouter.get("/feed", isAuthenticated, userFeed);
 
 export default userRouter;
